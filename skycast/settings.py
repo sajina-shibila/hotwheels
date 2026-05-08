fb5e233c68b67e978875f14658aac0d52c9aa8ca
shibila <sajinasalam347@gmail.com>
2026-05-08 10:58:54 +0530

    newest

--- skycast/settings.py
+++ skycast/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure--_2f!98n5-*g%@5c(tke)wi^91=$37)4ul-0z_tgy6*ji@dls3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*'
+]
 
 # Application definition
 
